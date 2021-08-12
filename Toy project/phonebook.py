8d79e98ba016fceb562157e8a6152af13c852e80
nkim <namunkim1202@gmail.com>
2021-08-13 00:37:02 +0900

    phonebook_input

---
+++ Toy project/phonebook.py
@@ -0,0 +1,32 @@
+
+#1단계: 입력값 받기
+#2단계: 유효성 체크
+#3단계: 처리로직
+#4단계: 결과 출력
+
+contact=[]
+#입력값 받아 contact 인스턴스 생성
+#def contact_setting():
+
+name = input("이름: ")
+phone = input("전화번호:")
+while True:
+    if phone.isdigit():
+        break
+
+    else:
+        print("숫자만 입력해주세요")
+
+contact.append =([name, phone])
+print(contact)
+
+      # return contact_setting
+
+#유효성 검사
+
+
+#받은 입력값 저장하기
+
+
+
+
--- Toy project/upanddown.py
+++ Toy project/upanddown.py
@@ -1,19 +1,25 @@
 import random
 #1부터 100까지 난수 입력
 numb=random.randrange(1,101,1)
-num = -1
+pnum = -1
 
 numb_total = 0
 
 print("게임시작")
 
 #숫자 넣기
-while(numb != num):
-    num = int(input("1~100사이의 숫자를 입력하시오:"))
 
-    if(num>numb):
-        print("down")
-    elif(num<numb):
-        print("up")
-    numb_total+=1
-print(numb_total, "번만에 정답을 맞추셨습니다.")
+while(numb != pnum):
+        num = int(input("1~100사이의 숫자를 입력하시오:"))
+
+        if (numb_total > 3):
+            print("5번 시도하셨슴당 게임오버")
+            break
+
+        if(pnum>numb):
+          print("down")
+        elif(pnum<numb):
+          print("up")
+          numb_total += 1
+
+            #print(numb_total, "번만에 정답을 맞추셨습니다.")
